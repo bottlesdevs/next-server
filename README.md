16a65e21eadbea8ee99c4b4450b8fbbb98d81b7b
Eduardo Flores <edfloreshz@gmail.com>
2025-01-05 23:50:21 +0100

    add: grpc server setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# next-server
+# Bottles Next Server
 The source code of the Next server.
+
+## Usage
+Call the server using [grpcurl](https://github.com/fullstorydev/grpcurl) or any gRPC client.
+
+```bash
+grpcurl -plaintext -proto ./proto/bottles.proto -d '{}' '[::1]:50052' bottles.Client.Health
+{
+  "ok": true
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use proto::bottles_server::{Bottles, BottlesServer};
+
+mod proto {
+    tonic::include_proto!("bottles");
+}
+
+#[derive(Debug, Default)]
+struct BottlesService;
+
+#[tonic::async_trait]
+impl Bottles for BottlesService {
+    async fn health(
+        &self,
+        request: tonic::Request<proto::BottlesRequest>,
+    ) -> Result<tonic::Response<proto::BottlesResponse>, tonic::Status> {
+        let request = request.get_ref();
+        println!("Received request: {:?}", request);
+        Ok(tonic::Response::new(proto::BottlesResponse { ok: true }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50052".parse().unwrap();
+    let service = BottlesService::default();
+    println!("Listening on {}", addr);
+    tonic::transport::Server::builder()
+        .add_service(BottlesServer::new(service))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
